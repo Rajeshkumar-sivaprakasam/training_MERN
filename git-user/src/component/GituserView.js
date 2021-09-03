2fdc0988ed51f202fe4976ed607bf39da8fc88c7
Rajeshkumar <rk.sivaprakasam@outlook.com>
2021-09-03 17:46:13 +0530

    Rounded corner added for image!

--- git-user/src/component/GituserView.js
+++ git-user/src/component/GituserView.js
@@ -1,3 +1,4 @@
+import { Typography } from "@material-ui/core";
 import React, { useState } from "react";
 
 export const GitUserView = () => {
@@ -6,7 +7,14 @@ export const GitUserView = () => {
   return (
     <>
       <h1>{user}</h1>
-      <img src={image} />
+      <img src={image} alt="Avatar" style={{ borderRadius: "50%" }} />
+      <Typography
+        variant="body1"
+        style={{ textAlign: "center", marginTop: 30 }}
+      >
+        This is the profile image of {user} and he is the one of a collabrator
+        in GitHub{" "}
+      </Typography>
     </>
   );
 };
