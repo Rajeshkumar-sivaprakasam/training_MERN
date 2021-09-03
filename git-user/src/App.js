085309fd7649b6b19a5ff0d3306dbf9e7a875b5c
Rajeshkumar <rk.sivaprakasam@outlook.com>
2021-09-03 12:33:07 +0530

    Route added in App.js

--- git-user/src/App.js
+++ git-user/src/App.js
@@ -1,10 +1,19 @@
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import { GitUserList } from "./component/GitUset";
 
 function App() {
   return (
     <div className="App">
-      <GitUserList />
+      {/* <Switch>
+        <GitUserList />
+      </Switch> */}
+      <Switch>
+        <Route exact path="/" component={GitUserList} />
+        {/* <Route path="/login" component={Login} /> */}
+
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
